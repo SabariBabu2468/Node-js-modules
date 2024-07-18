1337b56f776c13136de3172cf9988517be15276a
SabariBabu2468 <sabaribabu4848@gmail.com>
2024-07-18 18:18:43 +0000

    new changes

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-let num1=parseInt(process.argv[3]);
-let num2=parseInt(process.argv[6]);
-let result=num1+num2;
-console.log(`the sum of ${num1} and ${num2} is ${result}`);
+const math=require("./math.js")
+
+
+let num1=parseInt(process.argv[2]);
+let num2=parseInt(process.argv[3]);
+
+console.log(`the sum of ${num1} and ${num2} is ${math.add(num1,num2)}`);
+console.log(`the sum of ${num1} and ${num2} is ${math.sub(num1,num2)}`);
+console.log(`the sum of ${num1} and ${num2} is ${math.mul(num1,num2)}`);
+console.log(`the sum of ${num1} and ${num2} is ${math.div(num1,num2)}`);
