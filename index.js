a4cdfa8e35962e646115c358c90acc9d9ef13c84
SabariBabu2468 <sabaribabu4848@gmail.com>
2024-07-18 17:46:34 +0000

    adding changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let num1=parseInt(process.argv[3]);
 let num2=parseInt(process.argv[6]);
-console.log(`the sum of ${num1} and ${num2} is ${num1+num2}`);
+let result=num1+num2;
+console.log(`the sum of ${num1} and ${num2} is ${result}`);
